19d1a9700afb9d196e5a0b2fcb2f76a470dd194a
leon-mirai <leon.lee4230@gmail.com>
2024-10-28 18:29:15 +1000

    jellyfish

--- src/game/enemies/jellyfish.ts
+++ src/game/enemies/jellyfish.ts
@@ -6,12 +6,26 @@ export default class enemy_Jellyfish extends Enemy
 {
     floatRadius: number;
     floatSpeed: number;
+    frameIndex: number;
+    
 
     constructor(x: number, y: number, image) 
     {
         super(x, y, 50, 10, 0, image);
         this.floatRadius = 30;
         this.floatSpeed = 0.5;
-        
+    }
+
+    update(engine: Engine) 
+    {
+        this.floatAround();
+    }
+    
+    floatAround(): void {
+        const offsetX = (Math.random() - 0.5) * this.floatSpeed;
+        const offsetY = (Math.random() - 0.5) * this.floatSpeed;
+
+        this.x += offsetX;
+        this.y += offsetY
     }
 }
